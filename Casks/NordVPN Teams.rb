f7d793f4ced124b4406a398db9dc6a0974fea2a0
Raikkonen <info@raikkonen-media.com>
2020-08-13 16:59:10 +0300

    Create NordVPN Teams.rb

---
+++ Casks/NordVPN Teams.rb
@@ -0,0 +1,12 @@
+cask 'NordVPN Teams' do
+  version :latest
+  sha256 :no_check
+
+
+  url "https://downloads.nordteams.com/mac/latest/NordVPNTeams.pkg"
+  appcast 'hhttps://nordvpnteams.com/downloads/'
+  name 'NordVPN Teams'
+  homepage 'https://nordvpnteams.com/'
+
+  app 'NordVPN Teams.app'
+end
