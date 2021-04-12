860221410b9448d7ff19665bb547e347b774baed
raikkonenclub <69405209+raikkonenclub@users.noreply.github.com>
2021-04-12 10:51:27 +0300

    Update wickrpro.rb

--- Casks/wickrpro.rb
+++ Casks/wickrpro.rb
@@ -3,7 +3,7 @@ cask 'wickrpro' do
   sha256 :no_check
 
 
-  url "https://s3.amazonaws.com/static.wickr.com/downloads/mac/pro/WickrPro-5.75.16.dmg"
+  url "https://s3.amazonaws.com/static.wickr.com/downloads/mac/pro/WickrPro-5.76.13.dmg"
   appcast 'https://pro-download.wickr.com/api/multiVerify/pro/undefined/'
   name 'WickrPro'
   homepage 'https://wickr.com/products/pro/'
